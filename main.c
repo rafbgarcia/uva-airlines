ef3cae7515238ba1807c7e2ca40b3ca587f4ba85
Rafael Garcia <rafbgarcia@gmail.com>
2013-04-15 14:48:14 -0300

    Parte básica pronta, vou fazer a loǵica agora

--- main.c
+++ main.c
@@ -4,15 +4,17 @@
 #include "structs.h"
 #include "functions.h"
 
+struct input line;
 
-void scan(num_cities, num_direct_flights, num_queries) {
-  scanf("%d %d %d", &num_cities, &num_direct_flights, &num_queries);
+
+void test(City cities[], Flight direct_flights[], Query queries[]) {
+  printf("%s, %lf, %lf\n", cities[0].name, cities[0].lat, cities[0].lng);
+  printf("%s, %lf, %lf\n", cities[1].name, cities[1].lat, cities[1].lng);
+  printf("%s, %s\n", direct_flights[0].origin, direct_flights[0].destination);
+  printf("%s, %s\n", queries[0].origin, queries[0].destination);
 }
 
 int main(int argc, char ** argv) {
-  struct input line;
-  int i;
-
   scanf("%d %d %d", &line.cities, &line.direct_flights, &line.queries);
 
   while( ! end(line)) {
@@ -20,15 +22,14 @@ int main(int argc, char ** argv) {
     Flight direct_flights[line.direct_flights];
     Query  queries[line.queries];
 
+    /* Populate variables with inputs */
     scan_cities(cities, line.cities);
     scan_direct_flights(direct_flights, line.direct_flights);
     scan_queries(queries, line.queries);
 
-    printf("%s, %lf, %lf\n", cities[0].name, cities[0].lat, cities[0].lng);
-    printf("%s, %lf, %lf\n", cities[1].name, cities[1].lat, cities[1].lng);
-    printf("%s, %s\n", direct_flights[0].origin, direct_flights[0].destination);
-    printf("%s, %s\n", queries[0].origin, queries[0].destination);
 
+
+    /* Check next case */
     scanf("%d %d %d", &line.cities, &line.direct_flights, &line.queries);
   }
 
